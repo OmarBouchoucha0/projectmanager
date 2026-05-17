fadab7fcc1e4eda26c28dd603af09e4d0d7b311f
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-17 11:18:34 +0100

    feat: login page (less shit)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import Login from "@/components/auth/login"
 
 export default function Page() {
   return (<div className="flex items-center justify-center min-h-screen relative">
-    <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:18px_18px]" />
+    <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:96px_96px]" />
     <Login />
   </div>
   )
--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -12,31 +12,32 @@ import { Button } from "@/components/ui/button";
 
 export default function Login() {
   return (
-    <Card className="gap-6 py-2 border-border ">
+    <Card className="gap-8 pt-4 border-border pb-0 w-1/4 min-w-64">
       <CardHeader >
         <CardTitle>Login</CardTitle>
         <CardDescription>Use Credentials to login</CardDescription>
       </CardHeader>
       <CardContent>
         <form>
-          <div className="flex flex-col gap-2">
-            <div className="grid ">
+          <div className="flex flex-col gap-4">
+            <div className="grid gap-0">
               <Label htmlFor="email-rtl">email</Label>
               <Input
                 id="email-rtl"
                 type="email"
+                className="py-0"
                 required
               />
             </div>
 
             <div className="grid gap-0">
               <Label htmlFor="password-rtl">password</Label>
-              <Input id="password-rtl" type="password" required />
+              <Input id="password-rtl" type="password" className="py-0" required />
             </div>
           </div>
         </form>
       </CardContent>
-      <CardFooter className="flex-col gap-2 border-border border-t bg-muted">
+      <CardFooter className="flex-col gap-2 border-border border-t bg-muted pb-8">
         <Button type="submit" className="w-full">
           login
         </Button>
