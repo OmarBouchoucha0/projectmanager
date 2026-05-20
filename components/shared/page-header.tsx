f1876f583c6fd3d35487e1c357011c355675cf9c
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-20 18:28:54 +0100

    fix: page header padding

--- components/shared/page-header.tsx
+++ components/shared/page-header.tsx
@@ -5,7 +5,7 @@ import { HiddenTrigger } from "@/components/shared/app-sidebar-trigger"
 
 export function PageHeader() {
   return (
-    <header className="flex justify-between sticky top-0 w-full">
+    <header className="flex justify-between sticky top-0 w-full mb-4">
       <div className="flex items-center">
         <HiddenTrigger />
         <div className="text-xl px-2">Project Alpha</div>
