086726f5f14401625ae0283cf67b7c3f18b25f18
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-25 16:05:09 +0100

    feat: api call for login

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Card,
   CardContent,
@@ -10,22 +11,69 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc"
+import { useState } from "react";
 
 export default function Login({ onSwitch }: { onSwitch: () => void }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user/exists`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+        }
+      );
+
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        setError(data?.message || "Request failed");
+        return;
+      }
+
+      console.log("exists:", data.exists);
+
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Something went wrong");
+      console.error(error);
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
   return (
     <Card className="gap-8 pt-4 border-border border pb-0 min-w-84">
       <CardHeader >
         <CardTitle className="text-xl">Login</CardTitle>
         <CardDescription className="text-sm">Use Credentials to login</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
+      <form onSubmit={handleSubmit}>
+        <CardContent>
           <div className="flex flex-col gap-3">
             <div className="grid gap-0">
               <Label htmlFor="email-rtl">email</Label>
               <Input
                 id="email-rtl"
                 type="email"
+                value={email} onChange={(e) => setEmail(e.target.value)}
                 className="py-0"
                 required
               />
@@ -33,24 +81,29 @@ export default function Login({ onSwitch }: { onSwitch: () => void }) {
 
             <div className="grid gap-0">
               <Label htmlFor="password-rtl">password</Label>
-              <Input id="password-rtl" type="password" className="py-0" required />
+              <Input id="password-rtl"
+                type="password"
+                value={password} onChange={(e) => setPassword(e.target.value)}
+                className="py-0"
+                required />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 border-border border-t bg-muted pb-5">
-        <Button type="submit" className="w-full">
-          login
-        </Button>
-        <Button variant="outline" className="w-full">
-          <FcGoogle />
-          login With Google
-        </Button>
-        <p className="text-sm">
-          Does Not have an account?{" "}
-          <button onClick={onSwitch} className="underline">Sign up</button>
-        </p>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 border-border border-t bg-muted pb-5 mt-6">
+          {error && <p className="text-sm text-red-500">{error}</p>}
+          <Button type="submit" disabled={loading} className="w-full">
+            {loading ? "Login in up..." : "Login"}
+          </Button>
+          <Button variant="outline" className="w-full">
+            <FcGoogle />
+            login With Google
+          </Button>
+          <p className="text-sm">
+            Does Not have an account?{" "}
+            <button onClick={onSwitch} className="underline">Sign up</button>
+          </p>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
