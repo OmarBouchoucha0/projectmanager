987f1a8f04c21a530cc143a06cc5f1df7decf836
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-05-25 16:05:00 +0100

    feat: api call for signup

--- components/auth/signup.tsx
+++ components/auth/signup.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   Card,
   CardContent,
@@ -9,22 +10,70 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Signup({ onSwitch }: { onSwitch: () => void }) {
+  const [firstname, setFname] = useState("");
+  const [lastname, setLname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/user/create`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstname,
+            lastname,
+            email,
+            password,
+          }),
+        }
+      );
+
+      const message = await response.text();
+      if (!response.ok) {
+        setError(message || "Failed to create user");
+      }
+
+      console.log(message);
+
+    } catch (error) {
+      setError(error instanceof Error ? error.message : "Something went wrong");
+      console.error(error);
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <Card className="gap-8 pt-4 border-border border pb-0  min-w-84">
       <CardHeader >
         <CardTitle className="text-xl">Signup</CardTitle>
         <CardDescription className="text-sm">Create New Account</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
+
+      <form onSubmit={handleSubmit}>
+        <CardContent>
           <div className="flex flex-col gap-3">
             <div className="grid gap-0">
               <Label htmlFor="fname">First Name</Label>
               <Input
                 id="fname"
                 type="text"
+                value={firstname} onChange={(e) => setFname(e.target.value)}
                 className="py-0"
                 required
               />
@@ -35,6 +84,7 @@ export default function Signup({ onSwitch }: { onSwitch: () => void }) {
               <Input
                 id="lname"
                 type="text"
+                value={lastname} onChange={(e) => setLname(e.target.value)}
                 className="py-0"
                 required
               />
@@ -45,6 +95,7 @@ export default function Signup({ onSwitch }: { onSwitch: () => void }) {
               <Input
                 id="email"
                 type="email"
+                value={email} onChange={(e) => setEmail(e.target.value)}
                 className="py-0"
                 required
               />
@@ -52,20 +103,26 @@ export default function Signup({ onSwitch }: { onSwitch: () => void }) {
 
             <div className="grid gap-0">
               <Label htmlFor="password">password</Label>
-              <Input id="password" type="password" className="py-0" required />
+              <Input id="password"
+                type="password"
+                value={password} onChange={(e) => setPassword(e.target.value)}
+                className="py-0"
+                required />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex-col gap-2 border-border border-t bg-muted pb-6">
-        <Button type="submit" className="w-full">
-          Signup
-        </Button>
-        <p className="text-sm">
-          Already have an account?{" "}
-          <button onClick={onSwitch} className="underline">Login</button>
-        </p>
-      </CardFooter>
-    </Card>
+        </CardContent>
+        <CardFooter className="flex-col gap-2 border-border border-t bg-muted pb-6 mt-6">
+          {error && <p className="text-sm text-red-500">{error}</p>}
+          <Button type="submit" disabled={loading} className="w-full">
+            {loading ? "Signing up..." : "Signup"}
+          </Button>
+          <p className="text-sm">
+            Already have an account?{" "}
+            <button onClick={onSwitch} className="underline">Login</button>
+          </p>
+        </CardFooter>
+      </form>
+    </Card >
   );
 }
+
